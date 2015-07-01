88da46d4e94c4a247c60d7f7f5d7f1d8e0e05130
huhuikevin <huhuikevin@gmail.com>
2015-07-01 15:22:12 +0800

    add earth.h
    Signed-off-by: huhuikevin <huhuikevin@gmail.com>

---
+++ earth.h
@@ -0,0 +1,14 @@
+#ifndef __EARTH_H__
+#define __EARTH_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+extern int earthSetup(float r);
+extern void earthDrawSelf(int texId);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __EARTH_H__*/
